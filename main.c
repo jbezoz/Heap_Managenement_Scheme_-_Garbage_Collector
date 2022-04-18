18141ceea451d4ff6e264357b1fd618d8c889b83
Charudatta Badge <93867341+jbezoz@users.noreply.github.com>
2022-04-18 23:10:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,33 +5,21 @@
 
 int main() 
 {
+    struct Malloc_heap * head = NULL;
     memory_node * root = init_tree();
     memory_node * A;
     memory_node * B;
-    allocate(root, 8000, &A);
-    allocate(root, 8000, &B);
-    print_tree(root);
-    seperator();
-    A->block.state = FREE;
-    B->block.state = FREE;
+    if(1)
+    {
+        Malloc(root, 4000, &A, &head);
+        Malloc(root, 4000, &B, &head);
+        print_tree(root);
+        gc(head, root);
+    }
     collapse(root);
+    free_gc(head);
+    seperator();
     print_tree(root);
-
-    // struct Malloc_heap * head = NULL;
-    // memory_node * root = init_tree();
-    // memory_node * A;
-    // memory_node * B;
-    // if(1)
-    // {
-    //     Malloc(root, 4000, &A, &head);
-    //     Malloc(root, 4000, &B, &head);
-    //     print_tree(root);
-    //     gc(head, root);
-    // }
-    // collapse(root);
-    // free_gc(head);
-    // seperator();
-    // print_tree(root);
 
     return 0;
 }
